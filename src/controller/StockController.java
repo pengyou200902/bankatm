a2e26fa98b8a702398ea14cef2a8dd7b523f92e6
You Peng <youpeng@bu.edu>
2021-12-11 16:06:03 -0500

    add openAccount, closeAccount, todo transfer add StockController & TransactionController

---
+++ src/controller/StockController.java
@@ -0,0 +1,32 @@
+/**
+ * @author You Peng
+ * @date 12/11/2021 2:23 PM
+ */
+
+
+package controller;
+
+import dao.DaoFactory;
+import dao.StockDao;
+import model.OpResponse;
+import model.Security;
+import model.Stock;
+
+public class StockController {
+    private final StockDao stockDao;
+
+    public StockController() {
+        stockDao = DaoFactory.getStockDao();
+    }
+
+    public OpResponse buyStock(Security account, Stock stock) {
+        // TODO:
+        return null;
+    }
+
+    public OpResponse sellStock(Security account, Stock stock) {
+        // TODO:
+        return null;
+    }
+
+}
---
+++ src/controller/TransactionController.java
@@ -0,0 +1,21 @@
+/**
+ * @author You Peng
+ * @date 12/11/2021 2:09 PM
+ */
+
+
+package controller;
+
+import dao.DaoFactory;
+import dao.TransactionDao;
+
+public class TransactionController {
+    private final TransactionDao transactionDao;
+
+    public TransactionController() {
+        transactionDao = DaoFactory.getTransactionDao();
+    }
+
+
+
+}
